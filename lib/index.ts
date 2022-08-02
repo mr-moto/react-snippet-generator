ed3c01f241210c3af34b2b63050e960620897c6c
mr-moto <khash615@gmail.com>
2022-08-01 23:47:41 -0700

    fix: module missing error

--- lib/index.ts
+++ lib/index.ts
@@ -1,31 +1,31 @@
 #! /usr/bin/env node
 
-import { Command } from 'commander';
+import { Command } from "commander";
 const program = new Command();
 
-import { createFile } from '../util/createFile';
+import { createFile } from "../utils/createFile";
 
-program.name('rsg').version('0.0.1').description('Generate react components');
+program.name("rsg").version("0.0.1").description("Generate react components");
 
 program
-  .command('component')
-  .alias('c')
-  .description('Generate a new react component')
-  .argument('<name>', 'Component name')
-  .option('-t, --ts', 'typescript component', false)
+  .command("component")
+  .alias("c")
+  .description("Generate a new react component")
+  .argument("<name>", "Component name")
+  .option("-t, --ts", "typescript component", false)
   .action((name, options) => {
-    createFile(name, options, 'components');
+    createFile(name, options, "components");
   })
   .showHelpAfterError();
 
 program
-  .command('page')
-  .alias('p')
-  .description('Generate a new react page component')
-  .argument('<name>', 'Component name')
-  .option('-t, --ts', 'typescript component', false)
+  .command("page")
+  .alias("p")
+  .description("Generate a new react page component")
+  .argument("<name>", "Component name")
+  .option("-t, --ts", "typescript component", false)
   .action((name, options) => {
-    createFile(name, options, 'pages');
+    createFile(name, options, "pages");
   })
   .showHelpAfterError();
 
