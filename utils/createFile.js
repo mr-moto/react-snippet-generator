d873ea016f64128c672ef147f00bfd0b867e8850
mr-moto <khash615@gmail.com>
2022-08-13 23:56:38 -0700

    feat: config - directory path

--- utils/createFile.js
+++ utils/createFile.js
@@ -3,26 +3,42 @@ const mkdirp = require("mkdirp");
 const snippets = require("../snippets");
 const prettier = require("prettier");
 
+const { cosmiconfigSync } = require("cosmiconfig");
+
 module.exports.createFile = async (name, options, dir) => {
   const { ts } = options;
+
+  const explorerSync = cosmiconfigSync("rsg");
+  const { config } = explorerSync.search();
+
   const componentName = name.charAt(0).toUpperCase() + name.slice(1);
-  const componentDir = `${process.cwd()}/src/${dir}/${componentName}`;
+
   const extx = ts ? "tsx" : "jsx";
   const ext = ts ? "ts" : "js";
 
-  if (fs.existsSync(`${componentDir}`)) {
+  const componentDir = {
+    js: `${process.cwd()}/${
+      config[dir]?.path ?? `src/${dir}`
+    }/${componentName}`,
+    ts: `${process.cwd()}/${
+      config[dir]?.path ?? `src/${dir}`
+    }/${componentName}`,
+  };
+
+  if (fs.existsSync(`${componentDir[ext]}`)) {
     console.log(`${componentName} component directory already exists`);
     return false;
   }
   try {
-    await mkdirp(componentDir);
+    await mkdirp(componentDir[ext]);
+
     const prettierOptions = (await prettier.resolveConfig(
-      `${componentDir}/${componentName}.${ext}`
+      `${componentDir[ext]}/${componentName}.${ext}`
     )) || {
       singleQuote: true,
     };
 
-    prettierOptions.filepath = `${componentDir}/${componentName}.${extx}`;
+    prettierOptions.filepath = `${componentDir[ext]}/${componentName}.${extx}`;
 
     const fc = await prettier.format(
       snippets[ext](componentName).fc,
@@ -33,8 +49,8 @@ module.exports.createFile = async (name, options, dir) => {
       prettierOptions
     );
 
-    fs.writeFileSync(`${componentDir}/${componentName}.${extx}`, fc);
-    fs.writeFileSync(`${componentDir}/index.${ext}`, index);
+    fs.writeFileSync(`${componentDir[ext]}/${componentName}.${extx}`, fc);
+    fs.writeFileSync(`${componentDir[ext]}/index.${ext}`, index);
   } catch (error) {
     console.log(error);
     return false;
